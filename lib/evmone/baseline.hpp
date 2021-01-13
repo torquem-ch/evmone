f62d577b2a7571fbe1aa5a93af09e861f040c7fd
yperbasis <andrey.ashikhmin@gmail.com>
2021-01-13 10:46:13 +0100

    Allow calling baseline execution on execution state

--- lib/evmone/baseline.hpp
+++ lib/evmone/baseline.hpp
@@ -3,10 +3,13 @@
 // SPDX-License-Identifier: Apache-2.0
 #pragma once
 
+#include "execution_state.hpp"
 #include <evmc/evmc.h>
 
 namespace evmone
 {
 evmc_result baseline_execute(evmc_vm* vm, const evmc_host_interface* host, evmc_host_context* ctx,
     evmc_revision rev, const evmc_message* msg, const uint8_t* code, size_t code_size) noexcept;
+
+evmc_result baseline_execute_on_state(ExecutionState& state) noexcept;
 }  // namespace evmone
